4cc29d7520fb2c9a588b8bbf810951cd035d17f6
Devin Horvay <devin.c.horvay@gmail.com>
2022-07-14 23:22:31 -0400

    Update test_solution.py for 'Merge Two Sorted Lists'

--- tests/problem_0021/test_solution.py
+++ tests/problem_0021/test_solution.py
@@ -3,7 +3,7 @@ from leetcode.common.list_node import equals, from_list
 from leetcode.problem_0021 import Solution
 
 params = [
-    # nums, output
+    # list1, list2, output
     ((from_list([1,2,4]), from_list([1,3,4])), (from_list([1,1,2,3,4,4]))),
     ((from_list([]), from_list([])), (from_list([]))),
     ((from_list([]), from_list([0])), (from_list([0])))
