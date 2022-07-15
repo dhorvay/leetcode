df120d26ad645d10b917d16eb2f28ba1a3ae180a
Devin Horvay <devin.c.horvay@gmail.com>
2022-07-14 23:24:28 -0400

    Update test_solution.py for 'Reverse Linked List'

--- tests/problem_0206/test_solution.py
+++ tests/problem_0206/test_solution.py
@@ -3,7 +3,7 @@ from leetcode.common.list_node import equals, from_list
 from leetcode.problem_0206 import Solution
 
 params = [
-    # nums, output
+    # head, output
     ((from_list([1,2,3,4,5])), (from_list([5,4,3,2,1]))),
     ((from_list([1,2])), (from_list([2,1]))),
     ((from_list([])), (from_list([])))
